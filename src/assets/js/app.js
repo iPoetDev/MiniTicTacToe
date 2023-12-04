81497d7d7d71ead62e1413ed0c6075a9f2521e9a
Charles J. Fowler <ipoetdev-github-no-reply@outlook.com>
2023-12-04 22:07:50 +0000

    feat/deploy (source): AlpineJS Code Base
    Intent: Initialising CodeBase
    Issue:
    Fixed:
    Tag:
    Sprint:    01: Ends 23-12-10
    Bump:
    Previous:  23-12-06 v00.00.01.000
    Changelog: 23-12-06 v00.00.01.001
    - add:
      - source:
        - index.html
        - style.css
        - app.js
    - modified:
    - removed:
    - fixed:
    - linted:
    - edits:
    - deploy:
    
    Refs:
    -

---
+++ src/assets/js/app.js
@@ -0,0 +1,98 @@
+/* eslint-disable vue/html-self-closing,no-unused-vars */
+/**
+ * Represents a game app.
+ * @name app
+ * @returns {Object} An object with methods and properties for the app.
+ */
+function app() {
+    return {
+        turns: 0,
+        won: false,
+        winSeq: ['012', '345', '678', '036', '147', '258', '048', '246'],
+        grid: new Array(9).fill(null),
+        xChars: ['x', 'X'],
+        oChars: ['o', 'O'],
+        xTurns: '',
+        oTurns: '',
+
+        /**
+         * Selects a cell in the game grid.
+         *
+         * @function
+         * @name reset
+         * @memberof App
+         *
+         * @param {number} index - The index of the cell to select.
+         */
+        select: function (index) {
+            if (this.won || this.grid[index] !== null || this.turns >= 9)
+                return;
+
+            this.turns++;
+
+            if (this.turns % 2 === 0) {
+                this.grid[index] = this.xChars[Math.floor(
+                    Math.random() * this.xChars.length)];
+                this.xTurns += index;
+            } else {
+                this.grid[index] = this.oChars[Math.floor(
+                    Math.random() * this.oChars.length)];
+                this.oTurns += index;
+            }
+            this.checkWinner();
+        },
+
+        /**
+         * Checks if there is a winner by comparing the current turns of X and O with the winning sequences.
+         *
+         * @function
+         * @name checkWinner
+         * @memberof App
+         * @return {string} The winner ('X', 'O', or null if no winner yet).
+         */
+        checkWinner: function () {
+            for (let i = 0, length = this.winSeq.length; i < length; i++) {
+                if (new RegExp
+                        (`[${this.winSeq[i]}]{3}`).test(
+                            this.xTurns.replace(
+                                new RegExp(`[^${this.winSeq[i]}]+`,
+                                    'g'),
+                                    '')
+                            )
+                    )
+                    {
+                        this.won = 'X';
+                        break;
+                    }
+                else if (
+                    new RegExp(`[${this.winSeq[i]}]{3}`).test(
+                        this.oTurns.replace(
+                            new RegExp(`[^${this.winSeq[i]}]+`,
+                                'g'),
+                                '')))
+                    {
+                        this.won = 'O';
+                        break;
+                    }
+            }
+            return this.won;
+        },
+
+        /**
+         * Resets the game state.
+         * @function
+         * @name reset
+         * @memberof App
+         *
+         * @returns {void}
+         */
+        reset: function () {
+            this.turns = 0;
+            this.won = false;
+            this.grid = new Array(9).fill(null);
+            this.xTurns = '';
+            this.oTurns = '';
+        }
+
+    }
+}
